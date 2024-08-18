b3b2d973f8427639f76048a16e6bb26938baab2a
MohitKumar <mohitsaini25j@gmail.com>
2024-08-18 17:47:12 +0530

    fixed mongodb uri

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const {MONGO_URI} = require('../config/index');
 
 const connectDatabase = () => {
-    mongoose.connect("mongodb://localhost:27017/store", { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Mongoose Connected");
         });
